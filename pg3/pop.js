c2a1f48114ec3b74ab68842f882e155fe02d11cf
KhanStan <ithylight@gmail.com>
2017-03-05 19:02:06 +0200

    Updating prices...

---
+++ pg3/pop.js
@@ -0,0 +1,54 @@
+function getJSON(url) {
+    var resp ;
+    var xmlHttp ;
+
+    resp  = '' ;
+    xmlHttp = new XMLHttpRequest();
+
+    if(xmlHttp != null)
+    {
+    xmlHttp.open( "GET", url, false );
+        xmlHttp.send( null );
+        resp = xmlHttp.responseText;
+     }
+
+    return resp ;
+}
+
+
+
+var str1 = getJSON('old.json');
+var str2 = getJSON('new.json');
+
+var obj1 = jQuery.parseJSON(str1);
+var obj2 = jQuery.parseJSON(str2);
+
+var rows = "";
+
+for (var i = 0, len = Object.keys(obj1).length; i < len; i +=1) {
+    ind = [i+1];
+    key = Object.keys(obj1)[i];
+    yesterday = Number(obj1[key]['pop']);
+    today = Number(obj2[key]['pop']);
+
+    difference = function() {
+      var result;
+      var style;
+      result = today - yesterday;
+      if (result < 0) {
+        style = ' class = negative>' + result.toLocaleString();
+      } else if (result > 0){
+        style = ' class = positive> +' + result.toLocaleString();
+      } else {
+        style = '>' + result;
+      }
+      return style;
+    }
+
+    rows += '<tr><td>' + key + '</td><td>' + yesterday.toLocaleString() + '</td><td>' + today.toLocaleString() + '</td><td' + difference() + '</td></tr>';
+}
+
+
+jQuery(document).ready(function(){
+  $(rows).appendTo( "#dataTable" );
+});
