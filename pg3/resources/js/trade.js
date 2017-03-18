36a2c65da935348c396a0dcebbd11fb0f366aded
KhanStan <ithylight@gmail.com>
2017-03-18 03:54:11 +0200

    Added most used ports functionality

--- pg3/resources/js/trade.js
+++ pg3/resources/js/trade.js
@@ -21,18 +21,34 @@ var str = getJSON('resources/data.json') ;
 
 var obj = jQuery.parseJSON(str);
 
-var keysSorted = Object.keys(obj).sort(function(a,b){return obj[b]-obj[a]})
 
-
+var profitable_ports = {
+  'Tzogos-Tortuga': obj['Tzogos-Tortuga'],
+  'Regis-Vaasburg': obj['Regis-Vaasburg'],
+  'Tzogos-Aiora': obj['Tzogos-Aiora'],
+  'Tortuga-Aiora': obj['Tortuga-Aiora'],
+  'Seaglory-Aiora': obj['Seaglory-Aiora'],
+  'Seaglory-Vaasburg': obj['Seaglory-Vaasburg']
+}
 
 var rows = "";
+var rows2 = "";
 var ordKeys = Object.keys(obj).sort(function(a,b){return obj[b]-obj[a]}); // pass inside a function if you want specific order
+var ordKeys2 = Object.keys(profitable_ports).sort(function(a,b){return obj[b]-obj[a]});
 var key;
 var sliced = ordKeys.slice(0,20)
+// first loop - all ports.
 for (var i = 0, len = sliced.length; i < len; i +=1) {
-    key = sliced[i]
-    ind = [i+1]
+    key = sliced[i];
+    ind = [i+1];
     rows +=  "<tr class = 'myTableRow'><td class = 'myTableData'>"+ "#" + ind + ". " + '<td onclick="gg(this)" id = myTablePorts>' + key + "</td></td><td>" + '<img src="resources/gold_coins.gif" style = "width:14px;height:14px;" alt="MDN">  ' + obj[key] + "</td></tr>";
+}
+
+// second loop only proffitable.
+for (var i = 0, len = ordKeys2.length; i < len; i +=1) {
+  let key2 = ordKeys2[i];
+  let ind = [i+1];
+  rows2 += "<tr class = 'myTableRow'><td class = 'myTableData'>"+ "#" + ind + ". " + '<td onclick="gg(this)" id = myTablePorts>' + key2 + "</td></td><td>" + '<img src="resources/gold_coins.gif" style = "width:14px;height:14px;" alt="MDN">  ' + obj[key2] + "</td></tr>";
 }
 
 
@@ -55,7 +71,28 @@ function msToTime() {
     return console.log()
 }
 
+// handling switch between the 2 tables.
+var tableA = document.getElementById("itemList");
+var tableB = document.getElementById("itemList2");
+
+var btn = document.getElementById("switch");
+
+btn.onclick = function () {
+    if (tableA.style.display == "none") {
+      tableA.style.display = "table";
+      tableB.style.display = "none";
+      this.innerText = '[Display most used routes.]'
+    } else {
+      tableA.style.display = "none";
+      tableB.style.display = "table";
+      this.innerText = '[Display all routes]'
+    }
+}
+// end switch
+
+
 jQuery(document).ready(function(){
   $(rows).appendTo( "#itemList tbody" );
+  $(rows2).appendTo( "#itemList2 tbody" );
   msToTime();
 });
