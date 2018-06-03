f058f2928f221bb5522fa595fa9d128a5bfbd9cd
khanstan <ithylight@gmail.com>
2018-06-03 19:17:31 +0300

    test

--- diablo-database/grid.js
+++ diablo-database/grid.js
@@ -1,11 +1,69 @@
+function getJSON(url) {
+    var resp;
+    var xmlHttp;
+
+    resp  = '';
+    xmlHttp = new XMLHttpRequest();
+
+    if(xmlHttp != null)
+    {
+    xmlHttp.open( "GET", url, false );
+        xmlHttp.send( null );
+        resp = xmlHttp.responseText;
+     }
+
+    return resp;
+}
+
+
+var str = getJSON('uniques.json');
+
+var obj = JSON.parse(str);
+
+
 $(document).ready(function() {
     $('#example').DataTable({
-        "ajax": "grid_uniques.json",
+    	data: obj,
         "columns": [
-            {"data":"id"},
-            {"data":"uniqueName" },
-            {"data":"uniqueModifiers" }
+            {data:"id"},
+            null,
+            null,
+            null
         ],
-        mark: true,
+        columnDefs: [{
+        	"targets": 1,
+        	render: image
+        },{
+        	"targets": 2,
+        	render: main
+        },{
+        	"targets": 3,
+        	render: modifiers
+        }],
+        mark: true
     } )
 } );
+
+function modifiers(data, type, row) {
+	let req = row['uniqueRequirements']
+	let mod = row['uniqueModifiers']
+    return req + '</br><span class="modifiers">' + mod + '</span>'
+}
+
+function main(data, type, row) {
+	let name  = row['uniqueName']
+	let base = row['baseItem']
+	let _type = row['type']
+	let type2 = row['type2']
+	if (row['type'] == 'unique') {
+		return '<span class = "unique">' + name + '</span></br>' + base + '</br>	' + type2 
+	} else {
+		return name + '</br>' + base + '</br>' + type2
+	}
+        		//if row['type'] == unique change class color...
+	
+}
+
+function image(date, type, row) {
+	return '<img src="' + row['uniqueSrcIcon'] + '"></img>'
+}
